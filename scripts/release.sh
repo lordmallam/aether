ec42b3ebadbd4f210d4f4dcf71bc425f52ee67d5
lordmallam <lordy002000@gmail.com>
2018-10-16 12:39:34 +0100

    test

--- scripts/release.sh
+++ scripts/release.sh
@@ -43,6 +43,49 @@ release_app () {
     fi
 }
 
+function travis-branch-commit() {
+    if ! git checkout "$TRAVIS_BRANCH"; then
+        err "failed to checkout $TRAVIS_BRANCH"
+        return 1
+    fi
+
+    if ! git add --all .; then
+        err "failed to add modified files to git index"
+        return 1
+    fi
+    # make Travis CI skip this build
+    if ! git commit -m "Travis CI update [ci skip]"; then
+        err "failed to commit updates"
+        return 1
+    fi
+    # add to your .travis.yml: `branches\n  except:\n  - "/\\+travis\\d+$/"\n`
+    local git_tag=SOME_TAG_TRAVIS_WILL_NOT_BUILD+travis$TRAVIS_BUILD_NUMBER
+    if ! git tag "$git_tag" -m "Version increament tag from Travis CI build $TRAVIS_BUILD_NUMBER"; then
+        err "failed to create git tag: $git_tag"
+        return 1
+    fi
+    local remote=origin
+    if [[ $GITHUB_TOKEN ]]; then
+        remote=https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG
+    fi
+    if [[ $TRAVIS_BRANCH != master ]]; then
+        msg "not pushing updates to branch $TRAVIS_BRANCH"
+        return 0
+    fi
+    if ! git push --quiet --follow-tags "$remote" "$TRAVIS_BRANCH" > /dev/null 2>&1; then
+        err "failed to push git changes"
+        return 1
+    fi
+}
+
+function msg() {
+    echo "travis-commit: $*"
+}
+
+function err() {
+    msg "$*" 1>&2
+}
+
 # release version depending on TRAVIS_BRANCH/ TRAVIS_TAG
 if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+[\.0-9]*$ ]]
 then
@@ -65,6 +108,8 @@ fi
 
 echo "Release version:  $VERSION"
 echo "Release revision: $TRAVIS_COMMIT"
+
+travis-branch-commit
 
 # # Login in dockerhub with write permissions (repos are public)
 # docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
