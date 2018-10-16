a87edb2314696d40eb95435396b2235a153175d3
lordmallam <lordy002000@gmail.com>
2018-10-16 16:29:33 +0100

    test

--- scripts/release.sh
+++ scripts/release.sh
@@ -92,6 +92,7 @@ function travis-branch-commit() {
     #     msg "not pushing updates to branch $TRAVIS_BRANCH"
     #     return 0
     # fi
+    git push --quiet --follow-tags "$remote" "$TRAVIS_BRANCH"
     if ! git push --quiet --follow-tags "$remote" "$TRAVIS_BRANCH" > /dev/null 2>&1; then
         err "failed to push git changes"
         return 1
